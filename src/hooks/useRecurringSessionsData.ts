6f23941b6e51057a3baf45ef1043fcff9fd09c97
Jimmeey2323 <155805143+Jimmeey2323@users.noreply.github.com>
2025-11-01 16:34:50 +0530

    Update useRecurringSessionsData.ts

--- src/hooks/useRecurringSessionsData.ts
+++ src/hooks/useRecurringSessionsData.ts
@@ -42,9 +42,9 @@ export interface RecurringSessionData {
 }
 
 const GOOGLE_CONFIG = {
-  CLIENT_ID: "YOUR_CLIENT_ID_1.apps.googleusercontent.com",
-  CLIENT_SECRET: "GOCSPX-REDACTED_SECRET_1",
-  REFRESH_TOKEN: "REDACTED_REFRESH_TOKEN_1B",
+  CLIENT_ID: "YOUR_CLIENT_ID_2.apps.googleusercontent.com",
+  CLIENT_SECRET: "GOCSPX-REDACTED_SECRET_2",
+  REFRESH_TOKEN: "REDACTED_REFRESH_TOKEN_2",
   TOKEN_URL: "https://oauth2.googleapis.com/token"
 };
 
