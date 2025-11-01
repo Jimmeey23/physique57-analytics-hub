5d16d808d3eed7900a5055eedd1ed3fa26596dbc
Jimmeey2323 <155805143+Jimmeey2323@users.noreply.github.com>
2025-11-01 16:34:28 +0530

    Update useNewClientData.ts

--- src/hooks/useNewClientData.ts
+++ src/hooks/useNewClientData.ts
@@ -4,9 +4,9 @@ import { NewClientData } from '@/types/dashboard';
 import { parseDate } from '@/utils/dateUtils';
 
 const GOOGLE_CONFIG = {
-  CLIENT_ID: "YOUR_CLIENT_ID_1.apps.googleusercontent.com",
-  CLIENT_SECRET: "GOCSPX-REDACTED_SECRET_1",
-  REFRESH_TOKEN: "REDACTED_REFRESH_TOKEN_1B",
+  CLIENT_ID: "YOUR_CLIENT_ID_2.apps.googleusercontent.com",
+  CLIENT_SECRET: "GOCSPX-REDACTED_SECRET_2",
+  REFRESH_TOKEN: "REDACTED_REFRESH_TOKEN_2",
   TOKEN_URL: "https://oauth2.googleapis.com/token"
 };
 
